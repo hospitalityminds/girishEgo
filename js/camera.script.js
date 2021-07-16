2dee5110bc8eadbbcc6fce36bdffbf5e99f0f263
hospitalityminds <website.hospitalityminds@gmail.com>
2021-07-16 11:27:59 +0530

    first commit 🚀

---
+++ js/camera.script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_1').camera({
+				thumbnails: false,
+				height: '48%',
+				fx: 'simpleFade',
+				hover: false,
+				navigation: true
+				
+			});
+
+			
+		});
---
+++ js/owl.script.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+  
+    $("#owl-demo1").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
